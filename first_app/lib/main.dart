49d4185a24cd320198e0a6f57e45f322ad830000
arzenal <kirigayarz@gmail.com>
2025-08-19 17:03:26 +0800

    flutter first project for practice

---
+++ first_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/practice.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// With Flutter, you create user interfaces by combining "widgets"
+// You'll learn all about them (and much more) throughout this course!
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Every custom widget must have a build() method
+  // It tells Flutter, which widgets make up your custom widget
+  // Again: You'll learn all about that throughout the course!
+  @override
+  Widget build(BuildContext context) {
+    // Below, a bunch of built-in widgets are used (provided by Flutter)
+    // They will be explained in the next sections
+    // In this course, you will, of course, not just use them a lot but
+    // also learn about many other widgets!
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body: GradientContainer(),
+      ),
+    );
+  }
+}
