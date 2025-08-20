7ce0d024c6d36e0e04a44464548f941837368826
arzenal <kirigayarz@gmail.com>
2025-08-20 13:15:34 +0800

    stateful widget, lifting state management

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,23 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Quiz());
   }
 }
