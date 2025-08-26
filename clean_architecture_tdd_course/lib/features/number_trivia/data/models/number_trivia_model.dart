26302fb94bac40088212d0b4d7d1915f6eaa6ec4
arzenal <kirigayarz@gmail.com>
2025-08-26 16:46:03 +0800

    clean architecture update

--- clean_architecture_tdd_course/lib/features/number_trivia/data/models/number_trivia_model.dart
+++ clean_architecture_tdd_course/lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -5,6 +5,13 @@ class NumberTriviaModel extends NumberTrivia {
     : super(text: text, number: number);
 
   factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
-    return NumberTriviaModel(text: json['text'], number: json['number']);
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
   }
 }
